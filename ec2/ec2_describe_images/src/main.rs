d4c3102b99f9dd29cdf771531a45c7b091cfcabe
Rilindo Foster <rilindo@monzell-labs.net>
2020-11-20 21:03:56 -0600

    Added describe images

---
+++ ec2/ec2_describe_images/src/main.rs
@@ -0,0 +1,43 @@
+extern crate rusoto_core;
+extern crate rusoto_ec2;
+
+use rusoto_core::Region;
+use rusoto_ec2::{Ec2,
+    Ec2Client,
+    DescribeImagesRequest,
+    Filter
+};
+
+#[tokio::main]
+async fn main() {
+    let client = Ec2Client::new(Region::default());
+
+    let filter = Filter {
+        name: Some(String::from("is-public")),
+        values: Some(vec![String::from("false")])
+    };
+
+    let describe_images_request = DescribeImagesRequest {
+        filters: Some(vec![filter]),
+        ..Default::default()
+    };
+
+    match client.describe_images(describe_images_request).await {
+        Ok(output) => {
+            match output.images {
+                Some(images) => {
+                    for i in images {
+                        match i.name {
+                            Some(name) => println!("Name: {}, image id: {}", name, i.image_id.unwrap()),
+                            None => println!("Name: Unknown, image id: {}", i.image_id.unwrap())
+                        }
+                    }
+                }
+                None => println!("No images returned in this region")
+            }
+        }
+        Err(error) => {
+            println!("Error: {:?}", error);
+        }
+    }
+}
